5e226294e1f7ce434219b62aa5a6c20bcbb8a73b
JordaniYOV <timurmuhin80@gmail.com>
2026-05-17 16:57:37 +0300

    updated settings

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,14 +1,29 @@
 import secrets
 import picologging as logging
 from typing import Optional
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_congig = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=True,
+        extra="ignore",
+    )
+    # Project
     PROJECT_NAME: str = "Muse"
+    DEBUG: bool = False
+    ENV: str = "development"
 
+    # Security
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    # Database (deriver psycopg3 can work async and sync)
+    # DB_URL: str = f"postgresql+asyncpg://postgres:0508@localhost:5432/music"
+    # ASYNC_DB_URL: str = f"postgresql+asyncpg://postgres:1234@localhost:1234/muse"
+    ASYNC_DB_URL: str = f"postgresql+psycopg://postgres:1234@localhost:1234/muse"
 
     # Redis
     redis_url: str = "redis://localhost:12345"
@@ -20,15 +35,14 @@ class Settings(BaseSettings):
     redis_decode_response: bool = True
     cache_ttl: int = 3600
 
-    API_V1_STR: str = "/api/v1"
-    # DB_URL: str = f"postgresql+asyncpg://postgres:0508@localhost:5432/music"
-    # ASYNC_DB_URL: str = f"postgresql+asyncpg://postgres:1234@localhost:1234/muse"
-    ASYNC_DB_URL: str = f"postgresql+psycopg://postgres:1234@localhost:1234/muse"
+    # Kafka
+    kakfa_bootstrap_servers: str = "localhost:9094"
+    kafka_consumer_group: str = "muse-backend"
+    kafka_startup_timeout: int = 30
 
-    # LOGGER: logging.basicConfig = logging.basicConfig(
-    #     level=logging.INFO, 
-    #     format="%(asctime)s-%(name)s-%(levelname)s-%(message)s"
-    # )
+    # Router
+    API_V1_STR: str = "/api/v1"
+   
 
 
 settings = Settings()
